e8cb6683fac2fd84c7722d9ce37a9a482f47380c
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-01-11 23:41:01 -0600

    adding button effects and updating css

--- Assets/JavaScript.js
+++ Assets/JavaScript.js
@@ -1,12 +1,12 @@
-/* TomTom API key = ZpKOglbBbjaHIp34XAJCbc3fMUOpTKg6 */
+// /* TomTom API key = ZpKOglbBbjaHIp34XAJCbc3fMUOpTKg6 */
 
-if ("geolocation" in navigator) {
-    /* geolocation is available */
-    navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Latitude: " + position.coords.latitude);
-      console.log("Longitude: " + position.coords.longitude);
-    });
-  } else {
-    /* geolocation IS NOT available */
-    console.log("Geolocation is not available.");
-  }
+// if ("geolocation" in navigator) {
+//     /* geolocation is available */
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       console.log("Latitude: " + position.coords.latitude);
+//       console.log("Longitude: " + position.coords.longitude);
+//     });
+//   } else {
+//     /* geolocation IS NOT available */
+//     console.log("Geolocation is not available.");
+//   }
