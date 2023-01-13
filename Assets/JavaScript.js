798cd97f902b3bca9e95555b31b9be06c160b657
TonyWorldChanger <114870695+TonyWorldChanger@users.noreply.github.com>
2023-01-12 20:59:27 -0600

    fixed button hover

--- Assets/JavaScript.js
+++ Assets/JavaScript.js
@@ -2,9 +2,9 @@
 var locationButton = document.querySelector("button");
 
 
-$(locationButton).on("click", saveLocation);
+$(locationButton).on("click", getLocation);
     console.log("location");
-  function saveLocation() {
+  function getLocation() {
     if ("geolocation" in navigator) {
       /* geolocation is available */
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -21,10 +21,11 @@ $(locationButton).on("click", saveLocation);
   function waitForElement() {
     if(typeof longitude !== "undefined"){
       fetch ('https://api.tomtom.com/map/1/staticimage?key=ZpKOglbBbjaHIp34XAJCbc3fMUOpTKg6&center=' + longitude + ',' + latitude + '&layer=basic&style=night&zoom=12&width=1024&height=1024')
-      .then (res=>{$('#mapImg').attr('src', res.url)})
+          .then (res=>{$('#mapImg').attr('src', res.url)})
     
       } else {
           setTimeout(waitForElement, 250);
     }
   }
   
+  waitForElement(console.log("api"));
