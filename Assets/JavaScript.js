35ae2e9a2ae0ce967b314e44d7a85dac88a5932d
NotEnoughBacon <jboyk3@hotmail.com>
2023-01-16 17:59:27 -0700

    finsihed map

--- Assets/JavaScript.js
+++ Assets/JavaScript.js
@@ -1,7 +1,11 @@
 /* TomTom API key = ZpKOglbBbjaHIp34XAJCbc3fMUOpTKg6 */
 var locationButton = $("#locationBtn");
+tt.setProductInfo('A.D.B.L.O.W.', '69')
+
+
 
 function waitForElement(){
+  waitForElement()
   if(typeof longitude !== "undefined"){
     fetch ('https://api.tomtom.com/map/1/staticimage?key=ZpKOglbBbjaHIp34XAJCbc3fMUOpTKg6&center=' + longitude + ',' + latitude + '&layer=basic&style=night&zoom=12&width=1024&height=1024')
     .then (res=>{$('#mapImg').attr('src', res.url)})
@@ -14,11 +18,26 @@ function waitForElement(){
 function getLocation() {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
-    latitude = position.coords.latitude
-    longitude = position.coords.longitude
-    console.log(latitude)
-    console.log(longitude)
-    waitForElement()
+    globalThis.latitude = position.coords.latitude
+    globalThis.longitude = position.coords.longitude
+
+    var map = tt.map({
+      key: "ZpKOglbBbjaHIp34XAJCbc3fMUOpTKg6",
+      container: "map",
+      center: [longitude, latitude],
+      zoom: 11,
+      interactive: false,
+      style: {
+        map: 'basic_main',
+        trafficIncidents: 'incidents_day',
+        trafficFlow: 'flow_relative'
+      },
+      stylesVisibility: {
+        trafficFlow: true,
+        trafficIncidents: true,
+        map: true
+      }
+    })
     });
   } 
   else {
@@ -27,23 +46,4 @@ function getLocation() {
 }
 
 $(locationButton).on("click", getLocation);
-
-tt.setProductInfo('A.D.B.L.O.W.', '69')
-
-var map = tt.map({
-  key: "ZpKOglbBbjaHIp34XAJCbc3fMUOpTKg6",
-  container: "map",
-  center: [-114.2980608, 48.234496],
-  zoom: 12,
-  style: {
-    map: 'basic_main',
-    trafficIncidents: 'incidents_day',
-    trafficFlow: 'flow_relative'
-  },
-  stylesVisibility: {
-    trafficFlow: true,
-    trafficIncidents: true,
-    map: true
-  }
-})
 
